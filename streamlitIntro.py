cad36893fafacab2d599d92aa113c0137c52ae62
janhvis51 <119559319+janhvis51@users.noreply.github.com>
2023-12-04 23:13:20 +0530

    Add files via upload
    testing

---
+++ streamlitIntro.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.title("nmznbc")
+
+# #Write Text 
+# st.write("Text")
+
+# #Display an Image
+# st.image("content")
+
+# #Text Field
+# st.text_input("placeholder text","default value")
