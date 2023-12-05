80ce728e014da60a8f1d3bb5cc638a726f8211fc
janhvi singh <janhvis51@gmail.com>
2023-12-05 23:27:27 +0530

    update

--- streamlitIntro.py
+++ streamlitIntro.py
@@ -1,9 +1,9 @@
-# import streamlit as st
-# import pandas as pd
-# import numpy as np
-# import math
-# import random
-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+import random
+import requests
 import tensorflow as tf
 import streamlit as st
 st.title("Image Classification")
@@ -13,19 +13,19 @@ def load_model():
     return model
 #Title
 
-classes=["UnHealthy Syngonium podophyllum","UnHealthy Snake Plant","UnHealthy Adonidia merrillii","Healthy Syngonium podophyllum","Healthy Snake Plant","Healthy Adonidia merrillii"]
+classes=["UnHealthy Snake Plant","UnHealthy Syngonium podophyllum","UnHealthy Adonidia merrillii","Healthy Syngonium podophyllum","Healthy Snake Plant","Healthy Adonidia merrillii"]
 
 # image preprocessing
 def load_image(image):
-    img=tf.image.decode_jpeg(image,channels=3)
+    img=tf.image.decode_png(image,channels=3)
     img=tf.cast(img,tf.float32)
     img/=255.0
-    img=tf.image.resize(img,(28,28))
+    img=tf.image.resize(img,(256,256))
     img=tf.expand_dims(img,axis=0)
     return img
 
 #Get image URL from user
-image_path=st.text_input("Enter Image URL to classify...","snakeplant1.png")
+image_path=st.text_input("Enter Image URL to classify...")
 
 #Get image from URL and predict
 if image_path:
@@ -34,9 +34,21 @@ if image_path:
         st.write("Predicting Class...")
         with st.spinner("Classifying..."):
             img_tensor=load_image(content)
+            model = load_model()
             pred=model.predict(img_tensor)
             pred_class=classes[np.argmax(pred)]
             st.write("Predicted Class:",pred_class)
             st.image(content,use_column_width=True)
     except:
         st.write("Invalid URL")
+    # try:
+    #     content = requests.get(image_path).content
+    #     st.write("Predicting Class...")
+    #     with st.spinner("Classifying..."):
+    #         img_tensor = load_image(content)
+    #         model = load_model()
+    #         pred_class = model.predict(img_tensor)
+    #         st.write("Predicted Class:", pred_class)
+    #         st.image(content, use_column_width=True)
+    # except Exception as e:
+    #     st.write(f"Error: {e}")
