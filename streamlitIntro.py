d098562a37ce7a30514b587c65013615b344ce21
janhvi singh <janhvis51@gmail.com>
2023-12-05 14:01:07 +0530

    update requirement.txt

--- streamlitIntro.py
+++ streamlitIntro.py
@@ -4,10 +4,10 @@
 # import math
 # import random
 
-# st.write('Hi KUSHAL!')
 import tensorflow as tf
 import streamlit as st
 @st.cache(allow_output_mutation=True)
 def load_model():
     model=tf.keras.models.load_model('./second1.hdf5')
     return model
+st.write('Hi KUSHAL!')
