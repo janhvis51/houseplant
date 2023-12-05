2fc185d4cc1eb16297d1fd405cdcd4927c2f4177
janhvis51 <119559319+janhvis51@users.noreply.github.com>
2023-12-05 12:12:02 +0530

    Add files via upload

--- streamlitIntro.py
+++ streamlitIntro.py
@@ -1,13 +1,13 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import math
-import random
-
-st.write('Hi KUSHAL!')
-# import tersorflow as tf
 # import streamlit as st
-# @st.cache(allow_output_mutation=True)
-# def load_model():
-#     model=tf.keras.models.load_model('./second(1).hdf5')
-#     return model
+# import pandas as pd
+# import numpy as np
+# import math
+# import random
+
+# st.write('Hi KUSHAL!')
+import tensorflow as tf
+import streamlit as st
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model=tf.keras.models.load_model('./second(1).hdf5')
+    return model
